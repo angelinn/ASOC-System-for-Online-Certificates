a83d01991f424826f2479da3d7e59e551b1e0250
betrakiss <angelin.nedelchev@outlook.com>
2017-07-28 15:28:06 +0300

    Add Web API project. Add SusiService dependency injection.

---
+++ src/SusiAPI/SusiAPI.Web/Controllers/AffirmationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SusiAPI.Web.ViewModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SusiAPI.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class AffirmationController : Controller
+    {
+        private SusiService susiService;
+
+        public AffirmationController(SusiService susiService)
+        {
+            this.susiService = susiService;
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> GetAffirmation([FromBody]LoginViewModel login)
+        {
+            if (await susiService.LoginAsync(login.Username, login.Password))
+                return Ok();
+
+            return Ok();
+        }
+    }
+}
