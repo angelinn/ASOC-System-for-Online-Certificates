dd0b6dfdef39b159e175f3d8fd5ea4566079f597
betrakiss <angelin.nedelchev@outlook.com>
2017-08-05 13:44:07 +0300

    Add simple main page ui and LoginViewModel.

--- src/AffirmationBar/AffirmationBar/AffirmationBar/Views/MainPage.xaml.cs
+++ src/AffirmationBar/AffirmationBar/AffirmationBar/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AffirmationBar.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,12 @@ namespace AffirmationBar
 {
     public partial class MainPage : ContentPage
     {
+        public LoginViewModel LoginViewModel { get; set; } = new LoginViewModel();
         public MainPage()
         {
             InitializeComponent();
+
+            BindingContext = LoginViewModel;
         }
     }
 }
