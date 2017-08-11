aa9d65af3c2a0e97162caed322eb8bc1e6b5f366
betrakiss <angelin.nedelchev@outlook.com>
2017-08-11 22:26:53 +0300

    Add StudyYearConverter.

---
+++ src/AffirmationBar/AffirmationBar/AffirmationBar/Converters/StudyYearConverter.cs
@@ -0,0 +1,37 @@
+﻿using SusiAPICommon.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AffirmationBar.Converters
+{
+    public class StudyYearConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            StudyYear year = (StudyYear)value;
+            switch (year)
+            {
+                case StudyYear.First:
+                    return "първа";
+                case StudyYear.Second:
+                    return "втора";
+                case StudyYear.Third:
+                    return "трета";
+                case StudyYear.Fourth:
+                    return "четвърта";
+                default:
+                    return "Ексепшън...";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
