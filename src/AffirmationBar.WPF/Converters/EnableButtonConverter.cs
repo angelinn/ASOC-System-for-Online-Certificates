cac7b3580f412d11f751a81f3a2632ea8da5d872
Anelia <arblagoeva@gmail.com>
2017-08-13 20:05:24 +0300

    Disable button while loading

---
+++ src/AffirmationBar.WPF/Converters/EnableButtonConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AffirmationBar.WPF.Converters
+{
+	class EnableButtonConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			bool valueCame = (bool)value;
+			return !valueCame;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
