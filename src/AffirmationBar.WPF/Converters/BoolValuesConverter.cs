5b99671bea7ca6c3c69f7fc91242251307e09808
Anelia <arblagoeva@gmail.com>
2017-08-13 21:12:18 +0300

    Change name of bool value converter

--- src/AffirmationBar.WPF/Converters/BoolValuesConverter.cs
+++ src/AffirmationBar.WPF/Converters/BoolValuesConverter.cs
@@ -4,7 +4,7 @@ using System.Windows.Data;
 
 namespace AffirmationBar.WPF.Converters
 {
-	class EnableButtonConverter : IValueConverter
+	class BoolValuesConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
