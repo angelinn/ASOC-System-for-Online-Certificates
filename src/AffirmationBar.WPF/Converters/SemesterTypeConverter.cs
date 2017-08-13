63ae4f2f5fb53fa5c47b6fa846ca2462fbbdbe11
Anelia <arblagoeva@gmail.com>
2017-08-13 20:50:37 +0300

    Add semester type converter

---
+++ src/AffirmationBar.WPF/Converters/SemesterTypeConverter.cs
@@ -0,0 +1,34 @@
+﻿using SusiAPICommon.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AffirmationBar.WPF.Converters
+{
+	class SemesterTypeConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			Semester valueCame = (Semester) value;
+
+			switch (valueCame.Type)
+			{
+				case SemesterType.Summer:
+					return "Летен";
+				case SemesterType.Winter:
+					return "Зимен";
+				default:
+					return "Няма информация";
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
