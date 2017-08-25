1285a003da1ba97d03365ff534212953ee282bca
betrakiss <angelin.nedelchev@outlook.com>
2017-08-25 17:26:31 +0300

    Add CertificateOptionsPage.

---
+++ src/AffirmationBar.Mobile/AffirmationBar/AffirmationBar/Views/CertificateOptionsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using AffirmationBar.ViewModels;
+using SusiAPICommon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AffirmationBar.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CertificateOptionsPage : ContentPage
+    {
+        public CertificateOptionsViewModel CertificateOptionsViewModel { get; set; } = new CertificateOptionsViewModel();
+        public CertificateOptionsPage(StudentInfo info)
+        {
+            CertificateOptionsViewModel.Student = info;
+            CertificateOptionsViewModel.Selected = CertificateOptionsViewModel.Reasons[0];
+
+            BindingContext = CertificateOptionsViewModel;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/AffirmationBar.ViewModels/CertificateOptionsViewModel.cs
@@ -0,0 +1,39 @@
+﻿using SusiAPICommon.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AffirmationBar.ViewModels
+{
+    public class CertificateOptionsViewModel : BaseViewModel
+    {
+        public StudentInfo Student { get; set; }
+        public IList<string> Reasons { get; set; } = new List<string>()
+        {
+            "БДЖ",
+            "СКГТ (градски транспорт)"
+        };
+
+        public string FirstName
+        {
+            get
+            {
+                return Student.Names.Split(' ')[0];
+            }
+        }
+
+        private string selected;
+        public string Selected
+        {
+            get
+            {
+                return selected;
+            }
+            set
+            {
+                selected = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
