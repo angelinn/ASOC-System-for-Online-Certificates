8871e16f79d87f03bf8018a9873d4eb7042bc802
unknown <arblagoeva@gmail.com>
2017-08-28 17:08:03 +0300

    Add progress bar, download image on button click.

--- src/AffirmationBar.WPF/Views/StudentInfoWindow.xaml.cs
+++ src/AffirmationBar.WPF/Views/StudentInfoWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using AffirmationBar.ViewModels;
 using SusiAPICommon.Models;
+using System.Net;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace AffirmationBar.WPF.Views
@@ -9,6 +11,8 @@ namespace AffirmationBar.WPF.Views
 	/// </summary>
 	public partial class StudentInfoWindow : Window
 	{
+        LoginViewModel LoginViewModel { get; set; } = new LoginViewModel();
+
         public CertificateOptionsViewModel certificateOptions { get; set; } 
 		public StudentInfoWindow(StudentInfo studentInfo)
 		{
@@ -19,9 +23,18 @@ namespace AffirmationBar.WPF.Views
             this.DataContext = certificateOptions;
         }
 
-        private void getDoc_Click(object sender, RoutedEventArgs e)
+        private async Task GetCertificate()
         {
-            MessageBox.Show(certificateOptions.Student.Reason);
+            WebClient wb = new WebClient();
+            wb.DownloadFile("https://cdn-ssl.img.disneystore.com/content/ds/skyway/2014/category/full/fwb_frozen_20140110.jpg", @"D:\\test.jpeg");
+            string studentInfo = await LoginViewModel.GetCertificate();
         }
+
+        private async void getDoc_Click(object sender, RoutedEventArgs e)
+        {
+            await GetCertificate();
+        }
+
+        
     }
 }
