7f13ade4a9916a9e9b1fafb09a69bd0a37ac6e13
Anelia Blagoeva <arblagoeva@gmail.com>
2017-11-01 23:25:25 +0200

    Change a check if username and pass are correct.

--- src/AffirmationBar.WPF/Views/MainWindow.xaml.cs
+++ src/AffirmationBar.WPF/Views/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace AffirmationBar.WPF
 		{
 			LoginViewModel.Password = txtBoxPassword.Password;
 			StudentInfo studentInfo = await LoginViewModel.GetStudentInfoAsync();
-			if (studentInfo != null)
+			if (studentInfo.FacultyNumber != 0)
 			{
 				
 				var newForm = new StudentInfoWindow(studentInfo); //create your new form.
