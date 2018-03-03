a2ffcddcfbf942870df2f5fd3a875300beaa95db
unknown <arblagoeva@gmail.com>
2018-03-04 01:08:37 +0200

    Add choosing role/active faculty number window

---
+++ src/AffirmationBar.WPF/Views/ChoosingRoleWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using AffirmationBar.ViewModels;
+using SusiAPICommon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AffirmationBar.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for ChoosingRoleWindow.xaml
+    /// </summary>
+    public partial class ChoosingRoleWindow : Window
+    {
+        public RolesViewModel roles { get; set; }
+        public ChoosingRoleWindow(StudentInfo studentInfo)
+        {
+            InitializeComponent();
+
+            roles = new RolesViewModel(studentInfo);
+
+            this.DataContext = roles;
+        }
+
+        
+    }
+}
