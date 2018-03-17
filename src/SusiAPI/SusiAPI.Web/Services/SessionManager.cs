ea516b6d84b475f32b3690237ed5ce98d9eb1047
angelinn <angelin.nedelchev@outlook.com>
2018-03-18 01:40:18 +0200

    Keep student session for 5 minutes.

---
+++ src/SusiAPI/SusiAPI.Web/Services/SessionManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SusiAPI.Web.Services
+{
+    public class SessionEntry
+    {
+        public SusiSession Session { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+
+    public class SessionManager
+    {
+        private ConcurrentDictionary<string, SessionEntry> clients;
+        private Timer timer;
+
+        public SessionManager()
+        {
+            timer = new Timer((e) => Cleanup(), null, TimeSpan.Zero, TimeSpan.FromMinutes(5));
+        }
+
+        public void AddSession(string username, SusiSession service)
+        {
+            clients[username] = new SessionEntry
+            {
+                Session = service,
+                Expiration = DateTime.Now.AddMinutes(5)
+            };
+        }
+
+        public bool TryGetSession(string username, out SusiSession session)
+        {
+            session = null;
+            if (!clients.ContainsKey(username))
+                return false;
+
+            session = clients[username].Session;
+            return true;
+        }
+
+        private void Cleanup()
+        {
+            foreach (var tuple in clients)
+            {
+                if (DateTime.Now > tuple.Value.Expiration)
+                    clients.Remove(tuple.Key, out SessionEntry value);
+            }
+        }
+    }
+}
