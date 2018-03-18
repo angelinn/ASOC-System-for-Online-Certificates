a4e7499cdd1f3a4a6ce4ff9debff028a16ecf5e7
angelinn <angelin.nedelchev@outlook.com>
2018-03-18 02:25:25 +0200

    Consume token.

--- src/SusiAPIClient/SUSIClient.cs
+++ src/SusiAPIClient/SUSIClient.cs
@@ -3,6 +3,7 @@ using SusiAPI.Common.Models;
 using SusiAPICommon.Models;
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,8 @@ namespace SusiAPIClient
 {
     public class SusiClient
     {
-        private const string API_URL = "https://susiapi.azurewebsites.net";
-        private static readonly string LOGIN_URL = $"{API_URL}/api/login";
+        private const string API_URL = "http://localhost:61737";//"https://susiapi.azurewebsites.net";
+        private static readonly string TOKEN_URL = $"{API_URL}/api/token/create";
         private static readonly string STUDENT_INFO_URL = $"{API_URL}/api/affirmation";
         private static readonly string CERTIFICATE_URL = $"{API_URL}/api/affirmation/generate";
 
@@ -22,16 +23,13 @@ namespace SusiAPIClient
         private HttpClient client = new HttpClient();
         private bool isAuthenticated;
 
-        public async Task<StudentInfo> GetStudentInfoAsync(string username, string password)
+        public async Task<StudentInfo> GetStudentInfoAsync()
         {
             try
             {
-                string json = JsonConvert.SerializeObject(new { Username = username, Password = password });
+                string response = await client.GetStringAsync(STUDENT_INFO_URL);
 
-                HttpResponseMessage response = await client.PostAsync(STUDENT_INFO_URL, new StringContent(json, Encoding.UTF8, JSON_MEDIA_TYPE));
-                string resJson = await response.Content.ReadAsStringAsync();
-
-                SusiAPIResponseObject responseObject = JsonConvert.DeserializeObject<SusiAPIResponseObject>(resJson);
+                SusiAPIResponseObject responseObject = JsonConvert.DeserializeObject<SusiAPIResponseObject>(response);
                 if (responseObject.ResponseCode == SusiAPIResponseCode.Success)
                     return responseObject.Parse<StudentInfo>();
 
@@ -43,13 +41,11 @@ namespace SusiAPIClient
             }
         } 
 
-        public async Task<Certificate> GetCertificate(StudentInfo studentInfo)
+        public async Task<Certificate> GetCertificate(string reason)
         {
             try
             {
-                string json = JsonConvert.SerializeObject(studentInfo);
-
-                HttpResponseMessage response = await client.PostAsync(CERTIFICATE_URL, new StringContent(json, Encoding.UTF8, JSON_MEDIA_TYPE));
+                HttpResponseMessage response = await client.GetAsync(CERTIFICATE_URL + $"?reason={reason}");
                 string resJson = await response.Content.ReadAsStringAsync();
 
                 SusiAPIResponseObject responseObject = JsonConvert.DeserializeObject<SusiAPIResponseObject>(resJson);
@@ -62,6 +58,25 @@ namespace SusiAPIClient
             {
                 return null;
             }
+        }
+
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            string json = JsonConvert.SerializeObject(new { Username = username, Password = password });
+            HttpResponseMessage response = await client.PostAsync(TOKEN_URL, new StringContent(json, Encoding.UTF8, JSON_MEDIA_TYPE));
+
+            string resJson = await response.Content.ReadAsStringAsync();
+            SusiAPIResponseObject responseObject = JsonConvert.DeserializeObject<SusiAPIResponseObject>(resJson);
+            if (responseObject.ResponseCode == SusiAPIResponseCode.Success)
+            {
+                string token = responseObject.Data as string;
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                isAuthenticated = true;
+                return true;
+            }
+
+            return false;
         }
     }
 }
