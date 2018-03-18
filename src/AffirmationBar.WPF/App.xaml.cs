5872db372270cc47d459bb9cf54a7dd525ee5b41
angelinn <angelin.nedelchev@outlook.com>
2018-03-18 18:04:57 +0200

    Add hockey sdk to WPF.

--- src/AffirmationBar.WPF/App.xaml.cs
+++ src/AffirmationBar.WPF/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.HockeyApp;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,12 @@ namespace AffirmationBar.WPF
 	/// </summary>
 	public partial class App : Application
 	{
-	}
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            HockeyClient.Current.Configure("212665a3789d4a0198f3490fb9b7eef6");
+            await HockeyClient.Current.SendCrashesAsync();
+        }
+    }
 }
