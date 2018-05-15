3d97413d80fba281969e11c8f65c5d5b7c2a838b
angelinn <angelin.nedelchev@outlook.com>
2018-05-15 21:37:37 +0300

    Change android app name to ASOC.

--- src/AffirmationBar.Mobile/AffirmationBar/AffirmationBar.Android/MainActivity.cs
+++ src/AffirmationBar.Mobile/AffirmationBar/AffirmationBar.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using SusiAPICommon;
 
 namespace AffirmationBar.Droid
 {
-    [Activity(Label = "AffirmationBar", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ASOC", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
