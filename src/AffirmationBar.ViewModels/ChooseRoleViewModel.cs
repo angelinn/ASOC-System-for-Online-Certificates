b8eae6f0fdc5a4eeebb923c0962d552d7fc36b4a
angelinn <angelin.nedelchev@outlook.com>
2018-07-28 16:24:52 +0300

    Add base UI for choosing role.

---
+++ src/AffirmationBar.ViewModels/ChooseRoleViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AffirmationBar.ViewModels
+{
+    public class ChooseRoleViewModel : BaseViewModel
+    {
+        public List<string> Roles { get; set; } = new List<string>()
+        {
+            "71497",
+            "25771"
+        };
+
+        private string selectedRole;
+        public string SelectedRole
+        {
+            get
+            {
+                return selectedRole;
+            }
+            set
+            {
+                selectedRole = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async Task LoadRolesAsync()
+        {
+
+        }
+    }
+}
---
+++ src/AffirmationBar.WPF/Views/ChooseRoleWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using AffirmationBar.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AffirmationBar.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for ChooseRoleWindow.xaml
+    /// </summary>
+    public partial class ChooseRoleWindow : Window
+    {
+        public ChooseRoleViewModel ChooseRoleViewModel { get; private set; } = new ChooseRoleViewModel();
+        public ChooseRoleWindow()
+        {
+            InitializeComponent();
+
+            DataContext = ChooseRoleViewModel;
+        }
+    }
+}
